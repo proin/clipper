f64669ae0e614bf458ff86dbaba36c25a6737960
Chae Yeonghun <proin0312@gmail.com>
2015-05-20 21:49:55 +0900

    add fn

--- parser.js
+++ parser.js
@@ -1,4 +1,7 @@
 var selector = '';
+var remover = '';
+var innerFind = '';
+var attr = '';
 
 chrome.extension.onMessage.addListener(function (request, sender) {
     if (request.action == "getSource") {
@@ -11,17 +14,46 @@ function applySource(source) {
     var html = '<table class="table table-bordered">';
 
     $(source).find(selector).each(function () {
-        var finded = $(this).text();
+        var finded = '';
+        var tmp = $(this);
+        if (remover.length > 0)
+            $(this).find(remover).each(function () {
+                $(this).remove();
+            });
 
-        finded = finded.replace(/\t/gi, ' ');
-        finded = finded.replace(/\n/gi, ' ');
-        finded = finded.replace(/&nbsp;/gi, ' ');
-        while (finded.indexOf('  ') != -1) finded = finded.replace(/  /gi, ' ');
-        while (finded.startsWith(' ')) finded = finded.substring(1, finded.length);
-        while (finded.endsWith(' ')) finded = finded.substring(0, finded.length - 1);
+        if (attr == 'text') {
+            if (innerFind == null || innerFind.length == 0) {
+                finded = tmp.text();
+            } else {
+                tmp.find(innerFind).each(function () {
+                    finded += $(this).text() + ',';
+                });
+                if (finded.lastIndexOf(',') == finded.length - 1) finded = finded.substring(0, finded.length - 1);
+            }
+        } else {
+            if (innerFind == null || innerFind.length == 0) {
+                finded = tmp.attr(attr) + '';
+            } else {
+                tmp.find(innerFind).each(function () {
+                    finded += $(this).attr(attr) + ',';
+                });
+                if (finded.lastIndexOf(',') == finded.length - 1) finded = finded.substring(0, finded.length - 1);
+            }
+        }
 
-        if (finded.length > 1)
-            html += '<tr><td>' + finded + '</td></tr>';
+        console.log(finded);
+
+        if (finded.length > 0) {
+            finded = finded.replace(/\t/gi, ' ');
+            finded = finded.replace(/\n/gi, ' ');
+            finded = finded.replace(/&nbsp;/gi, ' ');
+            while (finded.indexOf('  ') != -1) finded = finded.replace(/  /gi, ' ');
+            while (finded.indexOf(' ') == 0) finded = finded.substring(1, finded.length);
+            while (finded.lastIndexOf(' ') == finded.length - 1) finded = finded.substring(0, finded.length - 1);
+
+            if (finded.length > 0)
+                html += '<tr><td>' + finded + '</td></tr>';
+        }
     });
 
     html += '</table>'
@@ -29,8 +61,23 @@ function applySource(source) {
 }
 
 function onWindowLoad() {
+    $('#selector-input').val(localStorage.selector);
+    $('#remover-input').val(localStorage.remover);
+    $('#attr-input').val(localStorage.attr);
+    $('#inner-input').val(localStorage.innerFind);
+
     $('#submit').click(function () {
+        $('#result').html('');
         selector = $('#selector-input').val();
+        remover = $('#remover-input').val();
+        attr = $('#attr-input').val();
+        innerFind = $('#inner-input').val();
+
+        localStorage.selector = selector;
+        localStorage.remover = remover;
+        localStorage.attr = attr;
+        localStorage.innerFind = innerFind;
+
         findList();
     });
 }
